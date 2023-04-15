0f8f39d5f5fc81fca3e89dddf834d286fa3e770c
thago-vale <consultoria.thiagovale@gmail.com>
2023-04-14 22:25:42 -0300

    up archives

---
+++ codes/desafio_read_and_write.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.append('../utils')
+from spark_config import spark_configs
+
+spark = spark_configs()
+
+spark = spark.builder.appName('read spark').getOrCreate()
+
+#Extract
+# cnaes = spark.read.format('csv').option('header','true').option('delimiter',';').load('../data/cnaes.csv')
+# estabelecimentos = spark.read.format('csv').option('header','true').option('delimiter',';').load('../data/estabelecimentos/')
+
+
+#Load
+# cnaes.write.format('parquet').mode('overwrite').save('../data/ceaes')
+# estabelecimentos.write.format('parquet').mode('overwrite').save('../data/estabelecimentos2')
